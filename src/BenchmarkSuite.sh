5d748cc248bac28086b1f6744047640b9550901d
de.sthy.university@gmail.com <de.sthy.university@gmail.com@bd12b014-eefb-f87f-39ab-9eccbbee6e7b>
2011-08-17 14:10:25 +0000

    git-svn-id: https://scala-benchmark-suite.googlecode.com/svn/trunk@19 bd12b014-eefb-f87f-39ab-9eccbbee6e7b

--- src/BenchmarkSuite.sh
+++ src/BenchmarkSuite.sh
@@ -5,15 +5,22 @@ exec > /dev/null
 # Created by ND P
 
 
+# Creates output class folder if necessary and invokes the default scala compiler to compile benchmark source file.
+# @param $1	The directory for bytecode class files.
+# @param $2	The benchmark source file name.
 function runCompile() {
-	if [ ! -d $1 ] ; then
-		mkdir $1
-	fi
+	[ -d $1 ] || mkdir $1
 	echo "scalac -d $1 $2"
-	echo Compiling...
-	scalac -d $1 $2 | goto RUN_BENCHMARK
+	echo 'Compiling...'
+	scalac -d $1 $2
 }
 
+# Invokes the harnesses .jar file to do benchmarking.
+# @param $1	The benchmark class name.
+# @param $2	The benchmark class directory.
+# @param $3	The iterator of warm up phase.
+# @param $4	The running iterator.
+# @param $5	The multiplier iterator.
 function runBenchmark() {
 	echo "scala BenchmarkDriver.jar $1 $2 $3 $4 $5"
 	echo "Benchmark running..."
@@ -21,6 +28,7 @@ function runBenchmark() {
 	scala BenchmarkDriver.jar $1 $2 $3 $4 $5 > $logfile
 }
 
+# Prints out the suite's usage.
 function printUsage() {
 	echo 'Usage: BenchmarkSuite -src <scala source file> -warmup <warm up> -runs <runs> [-multiplier <multiplier>] [-noncompile] [-classdi] <classdir>] [-help]'
 	echo '	The benchmark is warmed up <warm up> times, then run <runs> times, forcing a garbage collection between runs.'
@@ -30,10 +38,12 @@ function printUsage() {
 	echo '	The optional -help prints this usage.'
 }
 
+# Initialize variables
 multiplier=
 classdir=
 compile=
 
+# Parse commandline arguments
 while [ $# -gt 0 ] ; do
 	case $1 in 
 		"-help")
@@ -66,23 +76,33 @@ while [ $# -gt 0 ] ; do
 			compile=no
 			shift
 			;;
+		"-*")
+			echo "$0: error - unrecognized option $1" 1>&2
+			exit 1
+			;;
+		"*")
+			printUsage
+			;;
 	esac
 done
 
-if [ -z $src ] ; then
+if [ -z $src ] ; then			# No benchmark source file specified
 	printUsage
 	exit 1
-elif [ -z $warmup ] ; then
+elif [ -z $warmup ] ; then		# No warm up iterator specified
 	printUsage
 	exit 2
-elif [ -z $runs ] ; then
+elif [ -z $runs ] ; then		# No runs iterator specified
 	printUsage
 	exit 3
-elif [ -z $multiplier ] ; then
+elif [ -z $multiplier ] ; then	# No multiplier iterator specified, assign by 1
+	
 	multiplier=1
-	if [ -z $classdir ] ; then
-		classdir=$srcdrive$srcpath\\build
+	
+	if [ -z $classdir ] ; then				# No directory for bytecode class files specified
+		classdir=$srcdrive$srcpath\\build	# Set the default folder
 	fi
+	
 	if [ -n $compile ] ; then
 		# echo [Arguments] $classname $classdir $warmup $runs $multiplier $compile
 		runBenchmark $classname $classdir $warmup $runs $multiplier
@@ -91,6 +111,7 @@ elif [ -z $multiplier ] ; then
 		runCompile $classdir $src
 		runBenchmark $classname $classdir $warmup $runs $multiplier
 	fi
+	
 	echo "End of benchmarking."
 	exit 0
 fi
