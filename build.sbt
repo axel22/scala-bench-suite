c1804f07e2a78514c3d188417cc2ee5d79105df4
ngocduy pham <de.sthy.university@gmail.com>
2011-11-16 09:37:48 +0700

    Added ant task implement
    - added package ant

--- build.sbt
+++ build.sbt
@@ -15,3 +15,5 @@ libraryDependencies += "org.apache.commons" % "commons-math" % "2.2"
 libraryDependencies += "org.scalatest" % "scalatest_2.9.1" % "1.6.1"
 
 libraryDependencies += "javassist" % "javassist" % "3.12.1.GA"
+
+libraryDependencies += "org.apache.ant" % "ant" % "1.8.2"
---
+++ src/main/scala/scala/tools/sbs/ant/SBSTask.scala
@@ -0,0 +1,25 @@
+/*
+ * SBSTask
+ * 
+ * Version
+ * 
+ * Created on November 16th, 2011
+ * 
+ * Created by ND P
+ */
+
+package scala.tools.sbs
+package ant
+
+import org.apache.tools.ant.Task
+
+class SBSTask extends Task {
+
+  private var args: Array[String] = _
+  def setArgs(input: String) {
+    args = input split " "
+  }
+
+  override def execute() = BenchmarkDriver main args
+
+}
